8793b9c606fe1a1f1581ca7c103103c30c86a4ff
kubosho <ta2@o2p.jp>
2014-08-09 11:26:00 +0900

    feat(js): JPEG画像を透過にしてくれるJavaScriptを追加

---
+++ js/main-visual.js
@@ -0,0 +1,49 @@
+(function (exports) {
+    'use strict';
+
+    function CreateTransparentImages(targetTransparentImagePaths, maskImagesSuffix) {
+        var that = this;
+        var suffix = maskImagesSuffix || 'mask';
+
+        targetTransparentImagePaths.forEach(function (imagePath, i) {
+            var maskImagePath = imagePath.replace(/\.jpg/, '-' + suffix + '.png');
+            that.onLoadImage(imagePath, maskImagePath);
+        });
+    }
+
+    CreateTransparentImages.prototype.onLoadImage = function (targetImageName, maskImageName) {
+        var targetImage = document.createElement('img');
+        targetImage.src = targetImageName;
+
+        var maskImage = document.createElement('img');
+        maskImage.src = maskImageName;
+
+        targetImage.onload = this.translateImage(targetImage, maskImage);
+        maskImage.onload = this.translateImage(targetImage, maskImage);
+    };
+
+    CreateTransparentImages.prototype.translateImage = function (targetImage, maskImage) {
+        var canvas = document.createElement('canvas'),
+            ctx = canvas.getContext('2d');
+
+        canvas.width = targetImage.naturalWidth;
+        canvas.height = targetImage.naturalHeight;
+
+        ctx.drawImage(targetImage, 0, 0);
+        ctx.globalCompositeOperation = 'xor';
+        ctx.drawImage(maskImage, 0, 0);
+
+        return canvas;
+    };
+
+    CreateTransparentImages.prototype.appendCanvas = function (targetElement, canvas) {
+        document.querySelector(targetElement).querySelector(canvas);
+    };
+
+    exports.CreateTransparentImages = CreateTransparentImages;
+})(window);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var images = ['img/okabe.jpg', 'img/kurisu.jpg', 'img/kokuban.jpg'];
+    var transparentImages = new CreateTransparentImages(images);
+});
